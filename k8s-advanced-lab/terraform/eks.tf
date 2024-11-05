819c2de838d7652027e7e857d9d941920acd7418
Mohammed Sayed <mohammedsayed9607@gmail.com>
2024-11-05 19:40:55 +0000

    Fixed configuration issue

--- k8s-advanced-lab/terraform/eks.tf
+++ k8s-advanced-lab/terraform/eks.tf
@@ -1,18 +1,13 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-
-  cluster_name    = local.name
-  cluster_version = "1.31"
-
-  cluster_endpoint_public_access = true # in prod we don't do this
-
+  source                               = "terraform-aws-modules/eks/aws"
+  cluster_name                         = local.name
+  cluster_version                      = "1.31"
+  cluster_endpoint_public_access       = true
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
-  enable_irsa = true
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
+  enable_irsa                          = true
+  vpc_id                               = module.vpc.vpc_id
+  subnet_ids                           = module.vpc.private_subnets
+  control_plane_subnet_ids             = module.vpc.public_subnets
 
   eks_managed_node_group_defaults = {
     disk_size      = 50
--- k8s-advanced-lab/terraform/vpc.tf
+++ k8s-advanced-lab/terraform/vpc.tf
@@ -1,15 +1,15 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.9.0"
-  name = local.name
-  cidr = "10.0.0.0/16"
-  azs  = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  name    = local.name
+  cidr    = "10.0.0.0/16"
+  azs     = ["${local.region}a", "${local.region}b", "${local.region}c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
   public_subnet_tags = {
